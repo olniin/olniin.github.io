e063f23c243c9aaf5552535205f0407a31a735cd
olniin <182859982+olniin@users.noreply.github.com>
2025-11-17 18:38:54 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,27 @@
 /* ISC Scope main functionality */
+
+const button = document.querySelector("button");
+
+navigator.serial.addEventListener("connect", (e) => {
+  // Connect to `e.target` or add it to a list of available ports.
+});
+
+navigator.serial.addEventListener("disconnect", (e) => {
+  // Remove `e.target` from the list of available ports.
+});
+
+navigator.serial.getPorts().then((ports) => {
+  // Initialize the list of available ports with `ports` on page load.
+});
+
+button.addEventListener("click", () => {
+  const usbVendorId = 0xabcd;
+  navigator.serial
+    .requestPort({ filters: [{ usbVendorId }] })
+    .then((port) => {
+      // Connect to `port` or add it to the list of available ports.
+    })
+    .catch((e) => {
+      // The user didn't select a port.
+    });
+});
