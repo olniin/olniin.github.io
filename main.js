b1b309fc640146daf67bc58fe265ac36c0f7427d
olniin <182859982+olniin@users.noreply.github.com>
2026-01-24 23:18:39 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,3 +6,8 @@ if ("serial" in navigator) {
 } else {
   terminalElement.textContent = "No serial for you!! :(";
 }
+
+document.querySelector("button").addEventListener("click", async () => {
+  // Prompt user to select a serial port
+  const port = await navigator.serial.requestPort();
+});
