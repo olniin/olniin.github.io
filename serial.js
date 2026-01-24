27410b8652d83a6c25740aeca7e95d777f387977
olniin <182859982+olniin@users.noreply.github.com>
2026-01-24 23:43:42 +0200

    Create serial.js

---
+++ serial.js
@@ -0,0 +1,27 @@
+document.querySelector("button").addEventListener("click", async () => {
+  // Prompt user to select a serial port
+  const port = await navigator.serial.requestPort();
+});
+
+await port.open({baudRate: 115200});
+
+while (port.readable) {
+
+  const reader = port.readable.getReader();
+
+  try {
+    while (true) {
+      const {value, done} = await reader.read();
+      if (done) {
+        // allow port to be closed later
+        reader.releaseLock();
+        break;
+      }
+      if (value) {
+        console.log(value);
+      }
+    }
+  } catch (error) {
+    terminalElement.textContent(error);
+  }
+}
