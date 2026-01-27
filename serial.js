e9d0feb95ba57ae2a02b501fdd965a841307eb9c
olniin <182859982+olniin@users.noreply.github.com>
2026-01-27 16:05:24 +0200

    Update serial.js

--- serial.js
+++ serial.js
@@ -1,12 +1,14 @@
-export default async function serialConnect() {
+export async function serialConnect() {
   const port = await navigator.serial.requestPort();
 
   await port.open({baudRate: 115200});
 
+  const textDecoder = new TextDecoderStream();
+  const readableStreamClosed = port.readable.pipeTo(textDecoder.writable);
+  const reader = textDecoder.readable.getReader();
+
   while (port.readable) {
-
     const reader = port.readable.getReader();
-
     try {
       while (true) {
         const {value, done} = await reader.read();
@@ -16,11 +18,11 @@ export default async function serialConnect() {
           break;
         }
         if (value) {
-          console.log(value);
+          console.log(value); // should be a string now
         }
       }
     } catch (error) {
-      terminalElement.textContent(error);
+      // TODO: Handle non-fatal errors
     }
   }
 }
