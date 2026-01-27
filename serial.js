19a120069d607140011e49da4369be315e73bcd7
olniin <182859982+olniin@users.noreply.github.com>
2026-01-27 17:28:39 +0200

    Update serial.js

--- serial.js
+++ serial.js
@@ -47,17 +47,3 @@ class LineBreakTransformer {
     controller.enqueue(this.chunks);
   }
 }
-
-    // Append new chunks to existing chunks.
-    this.chunks += chunk;
-    // For each line breaks in chunks, send the parsed lines out.
-    const lines = this.chunks.split("\r\n");
-    this.chunks = lines.pop();
-    lines.forEach((line) => controller.enqueue(line));
-  }
-
-  flush(controller) {
-    // When the stream is closed, flush any remaining chunks out.
-    controller.enqueue(this.chunks);
-  }
-}
