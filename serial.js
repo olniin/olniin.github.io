6b2c0c38fb346b3454827120afab1c5e0e29dade
olniin <182859982+olniin@users.noreply.github.com>
2026-01-27 17:26:29 +0200

    Update serial.js

--- serial.js
+++ serial.js
@@ -1,13 +1,12 @@
 export async function serialConnect() {
   const port = await navigator.serial.requestPort();
+  await port.open({baudRate: 115200});
 
   const textDecoder = new TextDecoderStream();
   const readableStreamClosed = port.readable.pipeTo(textDecoder.writable);
   const reader = textDecoder.readable
     .pipeThrough(new TransformStream(new LineBreakTransformer()))
     .getReader();
-
-  await port.open({baudRate: 115200});
 
   while (port.readable) {
     try {
@@ -35,6 +34,20 @@ class LineBreakTransformer {
   }
 
   transform(chunk, controller) {
+    // Append new chunks to existing chunks.
+    this.chunks += chunk;
+    // For each line breaks in chunks, send the parsed lines out.
+    const lines = this.chunks.split("\r\n");
+    this.chunks = lines.pop();
+    lines.forEach((line) => controller.enqueue(line));
+  }
+
+  flush(controller) {
+    // When the stream is closed, flush any remaining chunks out.
+    controller.enqueue(this.chunks);
+  }
+}
+
     // Append new chunks to existing chunks.
     this.chunks += chunk;
     // For each line breaks in chunks, send the parsed lines out.
