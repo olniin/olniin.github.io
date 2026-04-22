f0abee3504f1e9879a061a33aeeff967b44475af
olniin <182859982+olniin@users.noreply.github.com>
2026-04-22 11:02:45 +0300

    Update scope.js

--- scope.js
+++ scope.js
@@ -334,8 +334,8 @@ function handleFrame(frameU8)
   const frameU16ch2 = new Uint16Array(u16Count);
 
   for (let i = 0; i < u16Count; i++) {
-    frameU16ch1[i] = (frameU8[i * 2] << 8) | frameU8[i * 2 + 1];
-    frameU16ch2[i] = (frameU8[i * 2 + 2] << 8) | frameU8[i * 2 + 3];
+    frameU16ch1[i] = (frameU8[i * 4] << 8) | frameU8[i * 4 + 1];
+    frameU16ch2[i] = (frameU8[i * 4 + 2] << 8) | frameU8[i * 4 + 3];
   }
 
   plotFrame(frameU16ch1, frameU16ch2);
